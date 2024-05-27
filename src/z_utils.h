b36a3318f5aec9f5baf17f28814c63666fc33e63
Lukas Straub <lukasstraub2@web.de>
2024-05-27 18:25:34 +0200

    Switch to using nolibc

--- src/z_utils.h
+++ src/z_utils.h
@@ -1,30 +1,21 @@
 #ifndef Z_UTILS_H
 #define Z_UTILS_H
 
-#include <stdlib.h>
-#include <stdarg.h>
-#include <alloca.h>
-#include <string.h>
+#include "nolibc.h"
 
-#define z_alloca	__builtin_alloca
+#define alloca	__builtin_alloca
 
-void	*z_memset(void *s, int c, size_t n);
-void	*z_memcpy(void *dest, const void *src, size_t n);
-
-void	z_vprintf(const char *fmt, va_list ap);
-void	z_vfdprintf(int fd, const char *fmt, va_list ap);
-void	z_printf(const char *fmt, ...)
-	__attribute__ ((format (printf, 1, 2)));
-void	z_fdprintf(int fd, const char *fmt, ...)
-	__attribute__ ((format (printf, 2, 3)));
-void	z_errx(int eval, const char *fmt, ...)
-	__attribute__ ((format (printf, 2, 3)));
-
-#ifdef Z_SMALL
-#  define z_errx(eval, fmt, ...) z_exit(eval)
-#  define z_printf(fmt, ...) do {} while(0)
-#  define z_fdprintf(fd, fmt, ...) do {} while(0)
-#endif
+__attribute__ ((format (printf, 2, 3)))
+static void z_errx(int eval, const char *fmt, ...)
+{
+		va_list ap;
+		fprintf(stderr, "error: ");
+		va_start(ap, fmt);
+		vfprintf(stderr, fmt, ap);
+		va_end(ap);
+		fprintf(stderr, "\n");
+		exit(eval);
+}
 
 #endif /* Z_UTILS_H */
 
